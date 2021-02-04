18e19de512bd2e16e5778dbcc3076d194d015d1f
Thiago Lobo <thiago.lobo@ibm.com>
2021-02-03 21:34:30 -0300

    creating master branch

--- routes/apiMain.js
+++ routes/apiMain.js
@@ -1,13 +1,13 @@
-var returnGit = require('../connectors/githubApi')
-const router = require('express').Router()
+var returnGit = require(`../connectors/githubApi`)
+const router = require(`express`).Router()
 
 //Main route with http return:
 router.get(`/`, async function (req, res) {
     try {
-        var returnData = await returnGit.searchGitRep()
+        // var returnData = await returnGit.searchGitRep()
         res.status(200).send(returnData)
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500).send(`Error message: ` + error)
     }
 })
 
