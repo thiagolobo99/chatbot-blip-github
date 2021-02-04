bd5e4043f37c921d7bd352776de97e1271dcf652
Thiago Lobo <thiago.lobo@ibm.com>
2021-02-04 09:22:19 -0300

    prod

--- routes/apiMain.js
+++ routes/apiMain.js
@@ -4,7 +4,7 @@ const router = require(`express`).Router()
 //Main route with http return:
 router.get(`/`, async function (req, res) {
     try {
-        // var returnData = await returnGit.searchGitRep()
+        var returnData = await returnGit.searchGitRep()
         res.status(200).send(returnData)
     } catch (error) {
         res.status(500).send(`Error message: ` + error)
